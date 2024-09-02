3352db98bcc12028525ae0fa82355f1ffd38b1a2
Yasaratcha <157775654+Halimaw-magpa-buhat@users.noreply.github.com>
2024-09-02 18:28:56 +0800

    Add files via upload

--- scripts/stage2.gd
+++ scripts/stage2.gd
@@ -2,6 +2,29 @@ extends Node
 
 
 
+func _on_s_2_lvl_1_pressed():
+	get_tree().change_scene_to_file("res://scenes/Stage-2 Level1.tscn")
 
-func _on_button_pressed():
+
+func _on_s_2_lvl_2_pressed():
+	get_tree().change_scene_to_file("res://scenes/Stage-2 Level2.tscn")
+
+
+func _on_s_2_lvl_3_pressed():
+	get_tree().change_scene_to_file("res://scenes/Stage-2 Level3.tscn")
+
+
+func _on_s_2_lvl_4_pressed():
+	get_tree().change_scene_to_file("res://scenes/Stage-2 Level4.tscn")
+
+
+func _on_s_2_lvl_5_pressed():
+	get_tree().change_scene_to_file("res://scenes/Stage-2 Level5.tscn")
+
+
+func _on_nxt_pressed():
+	pass # Replace with function body.
+
+
+func _on_back_pressed():
 	get_tree().change_scene_to_file("res://scenes/stage1.tscn")
